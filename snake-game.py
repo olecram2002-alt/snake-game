1800c89c6c9ed27c7b1cc1f6504b0bf422728b99
olecram2002-alt <olecram2002@gmail.com>
2025-11-21 19:34:02 -0500

    almost fully working
    missing body generation

--- snake-game.py
+++ snake-game.py
@@ -1,26 +1,42 @@
 import pygame
 from sys import exit
+from random import randint
 
-class game():
+class Game():
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption('Snake-Game')
         
         self.screen = pygame.display.set_mode((700,700))
         self.clock = pygame.time.Clock()
+        self.player = Snake(self)
         self.running = True
         self.score = 0
         self.state = 0
     #sprite groups
         self.menu_text_group = pygame.sprite.Group()
-        self.game_group = pygame.sprite.Group()
+        self.game_over_group = pygame.sprite.Group()
+
+        self.game_ui_group = pygame.sprite.Group()
+        self.game_obstacle_group = pygame.sprite.Group()
+        self.game_collectable_group = pygame.sprite.Group()
+        self.player_group = pygame.sprite.Group()
     #sprites
-        #menu
-        self.menu_text1 = text(100,'Snake',(32,36,2),(350,350))
-        self.menu_text2 = text(20,'Press space to start',(67,74,5),(350,600))
-        self.menu_text_group.add(self.menu_text1,self.menu_text2)
-        #game
-        self.score_text = text(50,f'Score : {self.score}',(32,36,2),(350,60))
-        self.game_group.add(self.score_text)
+        self.sprites= {'snake_0': self.player,
+                       'left wall':Wall([0,0]),'right wall':Wall([0,1]),'bottom wall':Wall([1,0]),'top wall':Wall([1,1]),
+                       'fruit':Fruit(randint(25,675),randint(95,675)),
+                       'menu text1':Text(100,'Snake',(32,36,2),(350,350)),
+                        'menu text2':Text(20,'Press space to start',(67,74,5),(350,600)),
+                        'score text':Text(50,f'Score : {self.score}',(32,36,2),(350,60)),
+                        'game over':Text(100,'Game over',(32,36,2),(350,350))}
+
+        self.menu_text_group.add(self.sprites['menu text1'],self.sprites['menu text2'])
+        self.game_over_group.add(self.sprites['game over'])
+
+        self.game_ui_group.add(self.sprites['score text'])
+        self.game_obstacle_group.add(self.sprites['left wall'],self.sprites['right wall'],self.sprites['bottom wall'],self.sprites['top wall'])
+        self.game_collectable_group.add(self.sprites['fruit'])
+        self.player_group.add(self.sprites['snake_0'])
 
     def run(self):
         while self.running:
@@ -45,8 +61,20 @@ class game():
             if self.state == 0:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        #starts game
+            #starts game
                         self.state = 1
+        #game inputs
+            if self.state == 1:
+                if event.type == pygame.KEYDOWN:
+            #movement
+                    if event.key == pygame.K_w:
+                        self.player.direction = [0,-1]
+                    if event.key == pygame.K_s:
+                        self.player.direction = [0,1]
+                    if event.key == pygame.K_a:
+                        self.player.direction = [-1,0]
+                    if event.key == pygame.K_d:
+                        self.player.direction = [1,0]
 
     def menu(self):
         self.input()
@@ -58,18 +86,104 @@ class game():
     def game(self):
         self.input()
         self.screen.fill((218, 232, 88))
-        self.game_group.draw(self.screen) 
+
+        self.game_ui_group.draw(self.screen)
+        self.game_obstacle_group.draw(self.screen)
+        self.game_collectable_group.draw(self.screen)
+        self.player_group.draw(self.screen)
+
+        self.player.update()
+
+        pygame.display.update()
+    
+    def game_over(self):
+        self.input()
+        self.screen.fill((218, 232, 88))
+        self.game_over_group.draw(self.screen)
 
         pygame.display.update()
 
-class text(pygame.sprite.Sprite):
+class Snake(pygame.sprite.Sprite):
+    def __init__(self,game:Game):
+        super().__init__()
+        self.image = pygame.Surface((30,30))
+        self.image.fill((32,36,2))
+        self.rect = self.image.get_rect(center=(350,350))
+
+        self.game = game
+        self.velocity = [5,5]
+        self.direction = [0,0]
+
+    def movement(self):
+        self.rect.centerx += self.velocity[0]*self.direction[0]
+
+        self.rect.centery +=self.velocity[1]*self.direction[1]
+
+    def collision_detect(self):
+        for sprite in self.game.game_obstacle_group:
+            if self.rect.colliderect(sprite.rect):
+                self.game.state = 2
+
+        for sprite in self.game.game_collectable_group:
+            if self.rect.colliderect(sprite.rect):
+                self.game.score += 1
+                self.game.sprites['score text'].update(f'Score : {self.game.score}',(32,36,2))
+                sprite.kill()
+
+                self.game.sprites['fruit'] = Fruit(randint(25,675),randint(95,675))
+                self.game.game_collectable_group.add(self.game.sprites['fruit'])
+
+    def update(self):
+        self.movement()
+        self.collision_detect()
+
+class Body(pygame.sprite.Sprite):
+    def __init__(self,game):
+        super().__init__()
+        self.image = pygame.Surface((30,30))
+        self.image.fill((32,36,2))
+        self.rect = self.image.get_rect(center=(350,350))
+
+        self.game = game
+        self.velocity = [5,5]
+        self.direction = [0,0]
+
+class Fruit(pygame.sprite.Sprite):
+    def __init__(self,x:int,y:int):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.image = pygame.Surface((20,20))
+        self.rect = self.image.get_rect(center=(self.x,self.y))
+        self.image.fill((67,74,5))
+    
+class Wall(pygame.sprite.Sprite):
+    def __init__(self,index:list):
+        super().__init__()
+        if index[0] == 0:
+            self.image = pygame.Surface((5,605))
+        
+        elif index[0] == 1:
+            self.image = pygame.Surface((680,5))
+
+        if index[1] == 0:
+            self.rect = self.image.get_rect(bottomleft=(10,690))
+
+        elif index[1] == 1:
+            self.rect = self.image.get_rect(topright=(690,85))
+
+        self.image.fill((32,36,2))
+
+class Text(pygame.sprite.Sprite):
     def __init__(self,font_size: int,text: str,color: tuple,position: tuple):
         super().__init__()
-
         self.font = pygame.font.Font('graphics/Pixeltype.ttf',font_size)
         self.image = self.font.render(text, False, color)
         self.rect = self.image.get_rect(center=position)
+    
+    def update(self,text:str,color:tuple):
+        self.image = self.font.render(text, False, color)
 
 if __name__ == '__main__':
-    snake = game()
+    snake = Game()
     snake.run()
